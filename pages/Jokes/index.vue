fa86db2189734253660dfc0e142979d12c72f7e1
anh leduc <71745533+anhleduc10101995@users.noreply.github.com>
2021-10-20 15:59:34 -0400

    first commit

---
+++ pages/Jokes/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>Jokes</div>
+</template>
+
+<script>
+export default {
+data() {
+  return {
+    jokes: []
+  }
+},
+async created() {
+  const config = {
+    headers: {
+      'Accept': 'application/json'
+    }
+  }
+  const res = await this.$axios.get('https://icanhazdadjoke.com/search', config)
+},
+head() {
+  return {
+      title: 'Dad Jokes',
+      meta: [
+          {
+              hid: 'description',
+              name: 'description',
+              content: 'Corny dad jokes'
+          }
+      ]
+  };
+    }
+};
+</script>
+
+<style>
+
+</style>
